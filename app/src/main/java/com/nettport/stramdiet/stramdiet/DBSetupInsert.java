04ae6fa86d78ea595db0ca2c39b469d039f47c8a
AtiaMobasshera <45450992+atiamobasshera@users.noreply.github.com>
2018-12-01 00:09:46 +0600

    Create README.md

--- app/src/main/java/com/nettport/stramdiet/stramdiet/DBSetupInsert.java
+++ app/src/main/java/com/nettport/stramdiet/stramdiet/DBSetupInsert.java
@@ -2,9 +2,6 @@ package com.nettport.stramdiet.stramdiet;
 
 import android.content.Context;
 
-/**
- * Created by bruker on 16.06.2017.
- */
 
 public class DBSetupInsert {
 
@@ -16,6 +13,83 @@ public class DBSetupInsert {
         this.context = ctx;
     }
 
+
+
+    /* Setup Insert To Categories ----------------------------------------- */
+    // To insert to category table
+    public void setupInsertToCategories(String values){
+        DBAdapter db = new DBAdapter(context);
+        db.open();
+        db.insert("categories",
+                "category_id, category_name, category_parent_id, category_icon, category_note",
+                values);
+        db.close();
+    }
+    public void insertAllCategories(){
+        setupInsertToCategories("NULL, 'Bread', '0', '', NULL");
+        setupInsertToCategories("NULL, 'Bread', '1', '', NULL");
+        setupInsertToCategories("NULL, 'Cereals', '1', '', NULL");
+        setupInsertToCategories("NULL, 'Frozen bread and rolls', '1', '', NULL");
+        setupInsertToCategories("NULL, 'Crispbread', '1', '', NULL");
+
+
+        setupInsertToCategories("NULL, 'Dessert and baking', '0', '', NULL");
+        setupInsertToCategories("NULL, 'Baking', '2', '', NULL");
+        setupInsertToCategories("NULL, 'Biscuit', '2', '', NULL");
+
+
+        setupInsertToCategories("NULL, 'Drinks', '0', '', NULL");
+        setupInsertToCategories("NULL, 'Soda', '3', '', NULL");
+
+
+        setupInsertToCategories("NULL, 'Fruit and vegetables', '0', '', NULL");
+        setupInsertToCategories("NULL, 'Frozen fruits and vegetables', '4', '', NULL");
+        setupInsertToCategories("NULL, 'Fruit', '4', '', NULL");
+        setupInsertToCategories("NULL, 'Vegetables', '4', '', NULL");
+        setupInsertToCategories("NULL, 'Canned fruits and vegetables', '4', '', NULL");
+
+
+        setupInsertToCategories("NULL, 'Health', '0', '', NULL");
+        setupInsertToCategories("NULL, 'Meal substitutes', '5', '', NULL");
+        setupInsertToCategories("NULL, 'Protein bars', '5', '', NULL");
+        setupInsertToCategories("NULL, 'Protein powder', '5', '', NULL");
+
+
+        setupInsertToCategories("NULL, 'Meat, chicken and fish', '0', '', NULL");
+        setupInsertToCategories("NULL, 'Meat', '6', '', NULL");
+        setupInsertToCategories("NULL, 'Chicken', '6', '', NULL");
+        setupInsertToCategories("NULL, 'Seafood', '6', '', NULL");
+
+
+        setupInsertToCategories("NULL, 'Dairy and eggs', '0', '', NULL");
+        setupInsertToCategories("NULL, 'Eggs', '7', '', NULL");
+        setupInsertToCategories("NULL, 'Cream and sour cream', '7', '', NULL");
+        setupInsertToCategories("NULL, 'Yogurt', '7', '', NULL");
+
+
+        setupInsertToCategories("NULL, 'Dinner', '0', '', NULL");
+        setupInsertToCategories("NULL, 'Ready dinner dishes', '8', '', NULL");
+        setupInsertToCategories("NULL, 'Pizza', '8', '', NULL");
+        setupInsertToCategories("NULL, 'Noodle', '8', '', NULL");
+        setupInsertToCategories("NULL, 'Pasta', '8', '', NULL");
+        setupInsertToCategories("NULL, 'Rice', '8', '', NULL");
+        setupInsertToCategories("NULL, 'Taco', '8', '', NULL");
+
+
+        setupInsertToCategories("NULL, 'Cheese', '0', '', NULL");
+        setupInsertToCategories("NULL, 'Cream cheese', '9', '', NULL");
+
+
+        setupInsertToCategories("NULL, 'On bread', '0', '', NULL");
+        setupInsertToCategories("NULL, 'Cold meats', '10', '', NULL");
+        setupInsertToCategories("NULL, 'Sweet spreads', '10', '', NULL");
+        setupInsertToCategories("NULL, 'Jam', '10', '', NULL");
+
+
+        setupInsertToCategories("NULL, 'Snacks', '0', '', NULL");
+        setupInsertToCategories("NULL, 'Nuts', '11', '', NULL");
+        setupInsertToCategories("NULL, 'Potato chips', '11', '', NULL");
+    }
 
 
 
