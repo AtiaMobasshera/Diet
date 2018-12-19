dc20be334f807d9758fade9d05fa0b1f060a5336
NadiaHaque <45225955+NadiaHaque@users.noreply.github.com>
2018-12-19 22:41:51 +0600

    Add files via upload

---
+++ app/src/main/java/com/nettport/stramdiet/stramdiet/SignUp.java
@@ -0,0 +1,17 @@
+package com.nettport.stramdiet.stramdiet;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by bruker on 17.06.2017.
+ */
+
+public class SignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_up);
+    }
+}
