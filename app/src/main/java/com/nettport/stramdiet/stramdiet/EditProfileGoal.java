0f299ca83bdd7fae6081fd0ac0532b84b725a75b
AtiaMobasshera <45450992+AtiaMobasshera@users.noreply.github.com>
2018-12-22 17:16:32 +0600

    Changes in Signup GOal

---
+++ app/src/main/java/com/nettport/stramdiet/stramdiet/EditProfileGoal.java
@@ -0,0 +1,17 @@
+package com.nettport.stramdiet.stramdiet;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by bruker on 19.06.2017.
+ */
+
+public class EditProfileGoal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.edit_profile_goal);
+    }
+}
