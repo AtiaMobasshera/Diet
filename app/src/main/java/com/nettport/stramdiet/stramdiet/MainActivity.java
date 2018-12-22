6b0b69b157153446275025553002cec7cbddc0e5
AtiaMobasshera <31773845+Atia5558@users.noreply.github.com>
2018-12-22 19:01:43 +0600

    updateMainActivity

--- app/src/main/java/com/nettport/stramdiet/stramdiet/MainActivity.java
+++ app/src/main/java/com/nettport/stramdiet/stramdiet/MainActivity.java
@@ -1,5 +1,7 @@
 package com.nettport.stramdiet.stramdiet;
 
+import android.content.Intent;
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
@@ -34,20 +36,27 @@ public class MainActivity extends AppCompatActivity {
 
         if(numberRows < 1){
             // Run setup
-            Toast.makeText(this, "Loading setup...", Toast.LENGTH_LONG).show();
+            // Toast.makeText(this, "Loading setup...", Toast.LENGTH_LONG).show();
             DBSetupInsert setupInsert = new DBSetupInsert(this);
             setupInsert.insertAllCategories();
             setupInsert.insertAllFood();
-            Toast.makeText(this, "Setup completed!", Toast.LENGTH_LONG).show();
+            // Toast.makeText(this, "Setup completed!", Toast.LENGTH_LONG).show();
 
         }
 
-
+        /* Check if there is user in the user table */
+        // Count rows in user table
+        numberRows = db.count("users");
+        if(numberRows < 1){
+            // Sign up
+            // Toast.makeText(this, "You are only few fields away from signing up...", Toast.LENGTH_LONG).show();
+            Intent i = new Intent(MainActivity.this, SignUp.class);
+            startActivity(i);
+        }
 
         /* Close database */
         db.close();
 
-
-
     }
+
 }
